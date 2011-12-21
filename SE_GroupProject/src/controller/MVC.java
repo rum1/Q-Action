00a30e87ac5ce2a149f315185ef0540c89867f01
Alex Aidoo - Micah <mic_alexis@yahoo.com>
2011-12-21 14:25:27 +0000

    This is the updater version

--- SE_GroupProject/src/controller/MVC.java
+++ SE_GroupProject/src/controller/MVC.java
@@ -5,6 +5,7 @@
 
 package controller;
 
+import model.*;
 import view.*;
 
 /**
@@ -13,15 +14,37 @@ import view.*;
  */
 public class MVC {
     private static CommandLine cmdLine;
-
+    private static CmdController cmdController;
+    private static Game gameModel;
+    
 
 
     public static void main(String[]args){
-        cmdLine = new CommandLine();
+        gameModel = new Game(); 
+        cmdLine = new CommandLine(); 
+        cmdController= new CmdController(gameModel, cmdLine); 
+        
+        MVC anMVC = new MVC(); 
+        anMVC.useConsoleView(); 
+    }
+
+    public void useConsoleView() {
         cmdLine.setName();
-
-
-
-
+        cmdLine.welcomeMessage();
+        cmdLine.setReady();
+        cmdLine.setSizeType();
+        
+        String type = cmdLine.getSizeType();
+        if(type.equalsIgnoreCase("n"))
+            cmdLine.setDimensions();
+        cmdController.calcDimensions();    
+        cmdController.getInputs();
+        cmdController.calcDimensions();
+        
+        cmdLine.setCommand();
+        String command = cmdLine.getCommand();
+        cmdController.cmdHandler(command);
+        
+        
     }
 }
--- SE_GroupProject/src/model/Game.java
+++ SE_GroupProject/src/model/Game.java
@@ -13,24 +13,41 @@ import java.util.ArrayList;
  */
 public class Game {
    // private Manual manualAccess;
+    private Game game;
     private BoardGame board;
     private Score scoreLine;
     private Die die;
-    private ArrayList<HumanPlayer> humanPlayers;
-    private ArrayList<Computer> cpuPlayers;
+    private ArrayList<Player> players;
+    
     private Player player;
+    private Manual manual; 
+    
+    private HumanPlayer humanPlyrsID; 
+    private Computer compPlyrsID; 
+    private String humanPlyrName; 
     
     public Game(){
        // manualAccess = new Manual();
         board = new BoardGame();
         scoreLine = new Score();
-        die = new Die();
-        humanPlayers = new ArrayList<HumanPlayer>();
-        cpuPlayers = new ArrayList<Computer>();
-        //player = new Player();
+        die = new Die(); 
+        players = new ArrayList<Player>();
+    }
+    
+    public void setPlayerID(){
+        int counter =0; 
+        for(Player x : players){
+            if(x instanceof HumanPlayer)
+                if(humanPlyrName.equalsIgnoreCase(((HumanPlayer)x).getPlayerName())){
+                    humanPlyrsID = (HumanPlayer)x;
+                    counter++; 
+                }
+                compPlyrsID = (Computer)players.get(counter+1);
+        }
     }
     
     public void save(){
+       
 //        for(int i=0; i<humanPlayers.size(); i++)
 //            String string = string+humanPlayers.get(i)+","+cpuPlayers.get(i)+";";
 //        
@@ -45,7 +62,7 @@ public class Game {
 //            JOptionPane.showMessageDialog(null, "this is an error");
 //        }
     }
-    public void load(){
+    public void load(String fileName){
 //        ArrayList<String> saves = new ArrayList<String>();
 //        String filename = "save.txt";
 //        try {
@@ -60,14 +77,35 @@ public class Game {
 //        	e.printStackTrace();
 //        }
     }
-    public void start(){
+    
+    
+    
+    public void start(String name){
+         
+        humanPlyrName = name;
+        game = new Game();
+         
+         int count =0; 
+         
+         players.add(new HumanPlayer(name));
+         players.add(new Computer());
+         
+         players.get(0).setToken(0);
+         players.get(1).setToken(0);
+         
+         
         //this.drawBoard(board, humanPlayers, cpuPlayers);
         //for(ArrayList<HumanPlayer> i: humanPlayer)
     }
-    public void end(){
+    public void quit(){
         
     }
+    
+    public void roll(){
+        die.roll();
+    }
     public void move(int x, int y){
+        
         //int x += die.getNumberRolled()
                 
     }
@@ -77,14 +115,35 @@ public class Game {
     public void bump(){
         //if(humanPlayer.get(i))
     }
-    public ArrayList<HumanPlayer> getHumanPlayer(){
-        return humanPlayers;
+    public ArrayList<Player> getPlayer(){
+        return players;
     }
-    public void cmdHandler(String cmd){
+    
+
+
+
+    
+
+    public void pause(){
+
+
+    }
+
+    public void addPlayer(Player aPlayer, String name){
+        aPlayer = new HumanPlayer(name);
+        players.add(player);
+        aPlayer = new Computer();
+        players.add(player);
+
+    }
+
+    public void resume() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void manCaller(String command, int length) {
+        manual = new Manual(command, length); 
         
-    }
-    public void setHumanPlayer(){
-       // humanPlayers.add()
     }
     
             
